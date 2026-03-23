11c81fc2f2953a96c7f52893603ff5becc4337f2
Juan David Hincapie Zarate <juanzarate1213@gmail.com>
2026-03-23 13:48:05 -0500

    corrección del LIMIT y el BETWEEN

--- SQL/01_practica_20_ejercicios.sql
+++ SQL/01_practica_20_ejercicios.sql
@@ -7,13 +7,13 @@ SELECT * FROM ventas_tech
 WHERE cantidad > 10;
 
 SELECT * FROM ventas_tech 
-WHERE precio_unitario > 100000 AND precio_unitario < 1000000;
+WHERE precio_unitario BETWEEN 100000 AND 1000000;
 
 SELECT * FROM ventas_tech 
 ORDER BY precio_unitario DESC;
 
 SELECT * FROM ventas_tech
-WHERE id_venta IN (1,2,3,4,5);
+LIMIT 5;
 
 SELECT DISTINCT categoria FROM ventas_tech
 ;

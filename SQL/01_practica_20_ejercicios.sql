abd1b833fd09932aeed481a81c96909e81bbb165
Juan David Hincapie Zarate <juanzarate1213@gmail.com>
2026-03-24 11:44:21 -0500

    finalizados todos lso ejercicios

--- SQL/01_practica_20_ejercicios.sql
+++ SQL/01_practica_20_ejercicios.sql
@@ -42,3 +42,47 @@ ORDER BY total_ingresos DESC;
 SELECT producto, SUM(cantidad) AS total_cantidad FROM ventas_tech
 GROUP BY producto
 ORDER BY total_cantidad DESC;
+
+SELECT producto, SUM(cantidad) AS total_cantidad FROM ventas_tech
+GROUP BY producto  
+HAVING total_cantidad > 15
+ORDER BY total_cantidad DESC;
+
+SELECT categoria, SUM(precio_unitario*cantidad) AS total_ingresos 
+FROM ventas_tech
+GROUP BY categoria
+HAVING total_ingresos > 5000000
+ORDER BY total_ingresos DESC;
+
+SELECT categoria, AVG(precio_unitario) AS precio_promedio 
+FROM ventas_tech
+GROUP BY categoria
+HAVING SUM(cantidad) > 5;
+
+SELECT producto, COUNT(*) AS total_ventas 
+FROM ventas_tech
+GROUP BY producto
+HAVING total_ventas > 3
+ORDER BY total_ventas DESC;
+
+SELECT fecha_venta, SUM(precio_unitario*cantidad) AS total_ingresos 
+FROM ventas_tech
+GROUP BY fecha_venta
+ORDER BY fecha_venta DESC;
+
+SELECT producto, SUM(cantidad) AS total_cantidad
+FROM ventas_tech
+WHERE categoria = 'Periféricos'
+HAVING SUM(cantidad*precio_unitario) > 500000
+ORDER BY SUM(cantidad*precio_unitario) DESC;
+
+/* La consulta anterior no funciona porque no se puede usar HAVING sin GROUP BY. 
+   Para corregirlo, debemos agregar GROUP BY producto antes de HAVING. */
+
+SELECT producto, SUM(cantidad) AS total_cantidad
+FROM ventas_tech
+WHERE categoria = 'Periféricos'
+GROUP BY producto -- ESTA ES LA PIEZA QUE FALTABA
+HAVING SUM(cantidad * precio_unitario) > 500000
+ORDER BY total_cantidad DESC
+LIMIT 1;

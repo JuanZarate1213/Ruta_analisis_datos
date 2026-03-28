5ad87a8b1773f774979b9e070555c012f6044ed8
Juan David Hincapie Zarate <juanzarate1213@gmail.com>
2026-03-28 13:02:15 -0500

    Done excercises 11-20 03_ejercicio_JOIN.sql

--- SQL/JOIN_relaciones/03_ejercicio_JOIN.sql
+++ SQL/JOIN_relaciones/03_ejercicio_JOIN.sql
@@ -34,5 +34,25 @@ SELECT COUNT(*) AS total_estudiantes FROM estudiantes;
 
 SELECT AVG(precio) AS precio_promedio FROM cursos;
 
-SELECT precio FROM cursos
+SELECT SUM(precio) AS ingresos_totales FROM cursos
+WHERE id_curso IN (SELECT id_curso FROM inscripciones);
+
+SELECT MIN(calificacion) AS calificacion_minima FROM inscripciones;
+
+SELECT pais, COUNT(*) AS total_estudiantes FROM estudiantes
+GROUP BY pais;
+
+SELECT categoria, MAX(precio) AS precio_maximo FROM cursos
 GROUP BY categoria;
+
+SELECT id_curso, ROUND(AVG(calificacion), 2) AS calificacion_promedio FROM inscripciones
+WHERE id_curso = 1;
+
+SELECT COUNT(*) AS total_inscripciones, id_curso FROM inscripciones
+GROUP BY id_curso;
+
+SELECT categoria, SUM(duracion_horas) AS horas_totales FROM cursos
+WHERE categoria = 'Idiomas';
+
+SELECT id_estudiante, ROUND(AVG(calificacion), 2) AS calificacion_promedio FROM inscripciones
+GROUP BY id_estudiante;

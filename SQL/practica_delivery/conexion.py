d3a92453942c1a5c6fc1d77fcaf42416842e6e47
Juan David Hincapie Zarate <juanzarate1213@gmail.com>
2026-04-03 13:02:37 -0500

    Actualización de conexion.py con los 10 primeros ejercicios

--- SQL/practica_delivery/conexion.py
+++ SQL/practica_delivery/conexion.py
@@ -3,19 +3,32 @@ import pandas as pd
 
 # Cambia la ruta por el formato correcto de Windows
 conn = sqlite3.connect(r'C:\Users\Juan Hincapie\Desktop\Universidad\Estudios_autodidactas\Ruta_analisis_datos\SQL\practica_delivery\delivery.db')
-query = """SELECT 
-                nombre, 
-                SUM(p.monto_total) AS total_gastado 
-            FROM usuarios AS u
-            JOIN pedidos AS p ON u.id_usuario = p.id_usuario
-            GROUP BY u.id_usuario
-            HAVING total_gastado > 200000
-            ORDER BY total_gastado DESC;"""
+query1 = """SELECT * FROM pedidos AS p
+            JOIN usuarios AS u ON p.id_usuario = u.id_usuario;"""
+query2 = """SELECT * FROM usuarios;"""
+df_pedidos = pd.read_sql_query(query1, conn) #1
+df_usuarios = pd.read_sql_query(query2, conn) 
 
-df_pedidos = pd.read_sql_query(query, conn)
+"""
+
+"""
 
 
 #print(df_pedidos.head())
-#print(df_pedidos.tail(10))
-print(df_pedidos.columns.tolist())
+
 conn.close()
+
+"""
+###BLOQUE 1###
+print(df_pedidos.tail(10)) #2
+print(df_pedidos.columns.tolist()) #3
+print(df_usuarios[['nombre', 'ciudad']]) #4
+print(df_pedidos[df_pedidos['monto_total'] > 50000]) #5
+print(df_pedidos[df_pedidos['ciudad'] == 'Cali']) #6
+print(df_pedidos['tiempo_entrega_min'].max()) #7
+print(len(df_pedidos))
+print(df_pedidos['tiempo_entrega_min'].count()) #8
+df_pedidos = df_pedidos.rename(columns={'monto_total': 'valor_factura'}) #9
+print(df_pedidos.dtypes) #10
+
+"""

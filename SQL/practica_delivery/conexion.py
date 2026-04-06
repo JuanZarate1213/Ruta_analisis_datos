8dd4b069279966580a3fc111d69d8d0d500ae622
Juan David Hincapie Zarate <juanzarate1213@gmail.com>
2026-04-05 22:02:51 -0500

    exercises 20 to 32 completed

--- SQL/practica_delivery/conexion.py
+++ SQL/practica_delivery/conexion.py
@@ -3,57 +3,98 @@ import pandas as pd
 
 # Cambia la ruta por el formato correcto de Windows
 conn = sqlite3.connect(r'C:\Users\Juan Hincapie\Desktop\Universidad\Estudios_autodidactas\Ruta_analisis_datos\SQL\practica_delivery\delivery.db')
-query1 = """SELECT * FROM pedidos AS p
-            JOIN usuarios AS u ON p.id_usuario = u.id_usuario
-            JOIN restaurantes AS r ON p.id_restaurante = r.id_restaurante;"""
-query2 = """SELECT * FROM usuarios;"""
-query3 = """SELECT * FROM restaurantes AS r;"""
-df_pedidos = pd.read_sql_query(query1, conn) #1
-df_usuarios = pd.read_sql_query(query2, conn) 
-df_restaurantes = pd.read_sql_query(query3, conn)
+query1 = """SELECT 
+                u.ciudad AS ciudad_u,
+                u.nombre AS nombre_u,
+                *
+                  FROM pedidos AS p
+            JOIN usuarios AS u ON p.id_usuario = u.id_usuario;"""
+df_p = pd.read_sql_query(query1, conn) #1
+df_only_p = pd.read_sql_query("""SELECT * FROM pedidos;""", conn)
+df_u= pd.read_sql_query("""SELECT * FROM usuarios;""", conn) 
+df_res = pd.read_sql_query("""SELECT nombre AS nombre_r,* FROM restaurantes AS r;""", conn)
+df_rep= pd.read_sql_query("""SELECT nombre AS nombre_rep,* FROM repartidores AS rep;""", conn)
 
 """
-###BLOQUE 3###
-print(df_pedidos['monto_total'].groupby(df_usuarios['ciudad']).mean().round(2)) #21
+###BLOQUE 4###
 
 
 """
-# 1. Une las tablas (Equivalente al JOIN de tu SQL)
-df_union = pd.merge(df_pedidos, df_restaurantes, on='id_restaurante')
 
-# 2. Agrupa sobre la tabla unida
-top_ventas = df_union.groupby('nombre')['monto_total'].sum().sort_values(ascending=False)
+#print(df_only_p['monto_total'].describe().round(2)) #30
 
-print(top_ventas)
+def clasificacion_monto(monto):
+    if monto < 22250:
+        return 'Bajo'
+    elif 22250 <= monto < 62750:
+        return 'Medio'
+    else:
+        return 'Alto'
+df_only_p['categoria_monto'] = df_only_p['monto_total'].apply(clasificacion_monto)
+print(df_only_p[['monto_total', 'categoria_monto']].head(10))
 
-#print(df_pedidos.head())
+#print(df_p.head())
 
 conn.close()
 
+
 """
 ###BLOQUE 1###
-print(df_pedidos.tail(10)) #2
-print(df_pedidos.columns.tolist()) #3
-print(df_usuarios[['nombre', 'ciudad']]) #4
-print(df_pedidos[df_pedidos['monto_total'] > 50000]) #5
-print(df_pedidos[df_pedidos['ciudad'] == 'Cali']) #6
-print(df_pedidos['tiempo_entrega_min'].max()) #7
-print(len(df_pedidos))
-print(df_pedidos['tiempo_entrega_min'].count()) #8
-df_pedidos = df_pedidos.rename(columns={'monto_total': 'valor_factura'}) #9
-print(df_pedidos.dtypes) #10
+print(df_p.tail(10)) #2
+print(df_p.columns.tolist()) #3
+print(df_u[['nombre_u', 'ciudad_u']]) #4
+print(df_p[df_p['monto_total'] > 50000]) #5
+print(df_p[df_p['ciudad_u'] == 'Cali']) #6
+print(df_p['tiempo_entrega_min'].max()) #7
+print(len(df_p))
+print(df_p['tiempo_entrega_min'].count()) #8
+df_p = df_p.rename(columns={'monto_total': 'valor_factura'}) #9
+print(df_p.dtypes) #10
 
 ###BLOQUE 2###
-print(df_pedidos['fecha_pedido'] = pd.to_datetime(df_pedidos['fecha_pedido'], errors='coerce')) #11
-print(df_pedidos['monto_con_propina'] = df_pedidos['monto_total'] * 1.10) #12
-print(print(df_pedidos.isnull().sum())) #13
-print(df_pedidos = df_pedidos.fillna(df_pedidos['tiempo_entrega_min'].mean())) #14
-print(df_pedidos['es_rapido'] = df_pedidos['tiempo_entrega_min'] < 30) #15
-print(df_pedidos['dia_semana'] = df_pedidos['fecha_pedido'].dt.day_name()) #16
-print(df_usuarios.drop(columns=['fecha_registro'], inplace=True)) #17
-print(df_pedidos[(df_usuarios['ciudad'] == 'Bogotá') & (df_pedidos['monto_total'] > 30000)]) #18
-print(df_pedidos['monto_total'].round(0)) #19
-print(df_usuarios['nombre'].str.upper()) #20
+print(df_p['fecha_pedido'] = pd.to_datetime(df_p['fecha_pedido'], errors='coerce')) #11
+print(df_p['monto_con_propina'] = df_p['monto_total'] * 1.10) #12
+print(print(df_p.isnull().sum())) #13
+print(df_p = df_p.fillna(df_p['tiempo_entrega_min'].mean())) #14
+print(df_p['es_rapido'] = df_p['tiempo_entrega_min'] < 30) #15
+print(df_p['dia_semana'] = df_p['fecha_pedido'].dt.day_name()) #16
+print(df_u.drop(columns=['fecha_registro'], inplace=True)) #17
+print(df_p[(df_u['ciudad_u'] == 'Bogotá') & (df_p['monto_total'] > 30000)]) #18
+print(df_p['monto_total'].round(0)) #19
+print(df_u['nombre_u'].str.upper()) #20
 
 """
 
+"""
+###BLOQUE 3###
+df_pedidos_usuarios = pd.merge(df_p, df_u, on='id_usuario')
+print(df_pedidos_usuarios.groupby('ciudad_u')['monto_total'].mean().round(2)) #21
+
+df_union = pd.merge(df_p, df_res, on='id_restaurante')
+print(top_ventas = df_union.groupby('nombre_r')['monto_total'].sum().sort_values(ascending=False).head(1)) #22
+
+df_pedidos_x_repartidor= pd.merge(df_p, df_rep , on='id_repartidor')
+print(df_pedidos_x_repartidor.groupby('nombre_rep').size().sort_values(ascending=False)) #23
+
+df_pedidos_x_repartidor= pd.merge(df_p, df_rep , on='id_repartidor')
+print(df_pedidos_x_repartidor.groupby('vehiculo')['tiempo_entrega_min'].mean().round(2).sort_values(ascending=False)) #24
+
+df_monto_x_res= pd.merge(df_p, df_res , on='id_restaurante')
+print(df_monto_x_res.groupby('especialidad')['monto_total'].agg(['max','min'])) #25
+
+df_p['fecha_pedido'] = pd.to_datetime(df_p['fecha_pedido'], errors='coerce')
+print(df_p.sort_values(by='fecha_pedido', ascending=False).head(10)) #26
+
+df_top_especialidad_ciudad= pd.merge(df_p, df_res , on='id_restaurante')
+print(df_top_especialidad_ciudad.groupby(['ciudad_u','especialidad'])['monto_total'].size().sort_values(ascending=False)) #27
+
+df_usuarios_top= pd.merge(df_only_p, df_u, on='id_usuario')
+print(df_usuarios_top.groupby(['id_usuario', 'nombre'])['monto_total'].size().sort_values(ascending=False).head(3)) #28 
+#siempre se agrupa por id_usuario para evitar confusiones con nombres repetidos, pero se muestra el nombre para mayor claridad.
+
+df_only_p['fecha_pedido'] = pd.to_datetime(df_only_p['fecha_pedido'], errors='coerce')
+df_diario = df_only_p.groupby('fecha_pedido')['monto_total'].sum().reset_index()
+df_diario['monto_acumulado_diario'] = df_diario['monto_total'].cumsum()
+
+print(df_only_p['tiempo_entrega_min'].describe().round(2)) #30
+"""

5bfdc634d957d44f586ff940f161fc73c04d101e
Juan David Hincapie Zarate <juanzarate1213@gmail.com>
2026-04-15 17:53:37 -0500

    feat: add new queries to consultas.sql

--- SQL/Postgres/consultas.sql
+++ SQL/Postgres/consultas.sql
@@ -1,4 +1,6 @@
+
 SELECT * FROM clientes;
+SELECT * FROM pedidos;
 
 SELECT * FROM clientes
 WHERE ciudad = 'Cali'
@@ -44,9 +46,61 @@ ORDER BY total_gastado DESC;
 
 SELECT 
     c.nombre, 
-    SUM(p.valor) AS total_gastado 
+    COALESCE(SUM(p.valor),0) AS total_gastado 
 FROM clientes AS c
-INNER JOIN pedidos AS p ON c.cliente_id = p.cliente_id
+LEFT JOIN pedidos AS p ON c.cliente_id = p.cliente_id
 GROUP BY nombre
-ORDER BY total_gastado DESC;    
+ORDER BY total_gastado DESC;  
+
+SELECT nombre FROM clientes
+WHERE cliente_id IN
+    (SELECT cliente_id FROM pedidos
+    WHERE valor > (
+        SELECT AVG(valor) FROM pedidos
+    )
+);
+
+WITH promedio AS (SELECT AVG(valor) AS avg_valor FROM pedidos), clientes_sobre_promedio AS (
+    (SELECT cliente_id FROM promedio, pedidos
+    WHERE valor > avg_valor)
+)
+SELECT nombre FROM clientes
+WHERE cliente_id IN (SELECT cliente_id FROM clientes_sobre_promedio);
+
+WITH suma_clientes AS (
+    SELECT cliente_id, SUM(valor) AS suma
+    FROM pedidos 
+    GROUP BY cliente_id), 
+    promedio_cliente AS (
+        SELECT AVG(suma) AS avg_s FROM suma_clientes),
+    resultado AS (SELECT cliente_id FROM suma_clientes, promedio_cliente
+    WHERE suma > avg_s)
+
+SELECT nombre FROM clientes
+WHERE cliente_id IN (SELECT cliente_id FROM resultado);
+
+SELECT nombre,
+    CASE 
+        WHEN ciudad = 'Cali' THEN 'Local'
+        WHEN ciudad = 'Bogotá' OR ciudad = 'Medellín' THEN 'Nacional'
+        ELSE 'Otro'
+    END AS envío
+FROM clientes;
+
+
+
+WITH gasto_cliente AS (
+    SELECT cliente_id, SUM(valor) AS suma_cliente FROM pedidos
+    GROUP BY cliente_id)
+
+SELECT suma_cliente, nombre,
+    CASE 
+        WHEN ciudad = 'Cali' THEN 'Local'
+        WHEN ciudad = 'Bogotá' OR ciudad = 'Medellín' THEN 'Nacional'
+        ELSE 'Otro'
+    END AS envío
+FROM clientes
+JOIN 
+ORDER BY suma_cliente;
+
 
